2f8668e8c28b0986fbbc2749f5d534f223e6ba27
omar16100 <omarshabab55@gmail.com>
2020-04-10 21:51:48 +0800

    adding s3 files

---
+++ js/config.js
@@ -0,0 +1,12 @@
+
+var _config = {
+    cognito: {
+        userPoolId: 'ap-south-1_eiJzUNebL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '458trp26sgqq4i12vr9s841v1u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1', // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'Base URL of your API including the stage', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
+    }
+};
+    
